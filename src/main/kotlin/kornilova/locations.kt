955e93de8d130083efecf2ae9b23c08139d245e1
Liudmila Kornilova <kornilova203@gmail.com>
2024-03-24 11:11:38 +0100

    Add flags

--- src/main/kotlin/kornilova/locations.kt
+++ src/main/kotlin/kornilova/locations.kt
@@ -4,11 +4,11 @@ import kornilova.SimpleLocation.entries
 import space.jetbrains.api.runtime.types.TD_Location
 import space.jetbrains.api.runtime.types.TD_MemberProfile
 
-enum class SimpleLocation(val id: String, val presentableName: String) {
-    BERLIN("1VSTug1k3zI8", "Berlin"),
-    NETHERLANDS("3fkpd53c1Sls", "Netherlands"),
-    MUNICH("1L51AV11kGh1", "Munich"),
-    PRAGUE("3FYOKl3LmPsO", "Prague"),
+enum class SimpleLocation(val id: String, val tag: String, val presentableName: String) {
+    BERLIN("1VSTug1k3zI8", "Berlin", "\uD83C\uDDE9\uD83C\uDDEA Berlin"),
+    NETHERLANDS("3fkpd53c1Sls", "Netherlands", "\uD83C\uDDF3\uD83C\uDDF1 Netherlands"),
+    MUNICH("1L51AV11kGh1", "Munich", "\uD83C\uDDE9\uD83C\uDDEA Munich"),
+    PRAGUE("3FYOKl3LmPsO", "Prague", "\uD83C\uDDE8\uD83C\uDDFF Prague"),
 }
 
 fun extractSimpleLocation(profile: TD_MemberProfile): SimpleLocation? {
@@ -27,7 +27,7 @@ fun extractSimpleLocation(profile: TD_MemberProfile): SimpleLocation? {
 
 fun extractLocationTags(profile: TD_MemberProfile): Set<String> {
     val simpleLocation = extractSimpleLocation(profile)
-    return if (simpleLocation != null) setOf(simpleLocation.presentableName) else emptySet()
+    return if (simpleLocation != null) setOf(simpleLocation.tag) else emptySet()
 }
 
 fun findById(id: String): SimpleLocation? {
