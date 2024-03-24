8fc557aa9de02b29657b82362fe85f832f365fb0
Liudmila Kornilova <kornilova203@gmail.com>
2024-03-24 10:59:30 +0100

    Paint first letter with mnemonic color

--- src/main/kotlin/kornilova/attributes.kt
+++ src/main/kotlin/kornilova/attributes.kt
@@ -1,7 +1,16 @@
 package kornilova
 
+import java.awt.Color
+
 // id has to be first because Anki uses first field as an id when re-importing notes with the update option turned on
-val colleagueAttributes = listOf(ColleagueId, FirstName, LastName, Picture, Memberships, Location)
+val colleagueAttributes = listOf(
+    ColleagueId,
+    ColoredFirstCharacter(FirstName),
+    ColoredFirstCharacter(LastName),
+    Picture,
+    Memberships,
+    Location
+)
 
 sealed interface ColleagueAttribute {
     fun get(colleague: Colleague): String
@@ -37,3 +46,36 @@ data object Memberships : ColleagueAttribute {
 data object Location : ColleagueAttribute {
     override fun get(colleague: Colleague): String = colleague.location ?: ""
 }
+
+
+data class ColoredFirstCharacter(val attribute: ColleagueAttribute) : ColleagueAttribute {
+    override fun get(colleague: Colleague): String {
+        val text = attribute.get(colleague)
+
+        val color = chooseColor(text[0])
+        return if (color != null) text.substring(0, 1).bold().color(color) + text.substring(1)
+        else text
+    }
+}
+
+fun String.bold(): String = "<b>$this</b>"
+
+fun String.color(color: Color): String {
+    return "<span style='color: rgb(${color.red}, ${color.green}, ${color.blue});'>$this</span>"
+}
+
+private fun chooseColor(c: Char): Color? {
+    return when (c) {
+        'А', 'И' -> Color(255, 0, 0)
+        'Б', 'Г', 'О', 'У' -> Color(60, 60, 255)
+        'К', 'П', 'Т', 'Ф', 'Х' -> Color(160, 160, 160)
+        'Л', 'М', 'Н' -> Color(56, 186, 78)
+        'С' -> Color(255, 200, 0)
+        'Р', 'Я' -> Color(228, 45, 228)
+        'Д' -> Color(112, 42, 37)
+        'Е', 'Ë' -> Color(50, 42, 174)
+        else -> null
+    }
+}
+// «а», «б», «в», «г», «д», «е», «ё», «ж», «з», «и», «й», «к», «л», «м», «н», «о», «п», «р», «с», «т», «у»,
+// «ф», «х», «ц», «ч», «ш», «щ», «ъ», «ы», «ь», «э», «ю», «я»
