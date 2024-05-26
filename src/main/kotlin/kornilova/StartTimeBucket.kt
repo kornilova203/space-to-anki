049ae2ad9d8f70d2806436e77deae4bac61dfaac
Liudmila Kornilova <kornilova203@gmail.com>
2024-05-26 16:01:49 +0200

    Sane experience at jb tags

--- src/main/kotlin/kornilova/StartTimeBucket.kt
+++ src/main/kotlin/kornilova/StartTimeBucket.kt
@@ -8,14 +8,12 @@ import java.util.*
 val calendar: Calendar = Calendar.getInstance()
 val today = LocalDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.DATE))
 
-enum class StartTimeBucket(val daysCount: Int, val color: Color?) {
-    BUCKET_0(0, null),
-    BUCKET_1(365, Color(56, 186, 78)),
-    BUCKET_2(365*3, Color(250, 231, 28)),
-    BUCKET_3(365*5, Color(255, 147, 39)),
-    BUCKET_4(365*10, Color(255, 0, 0));
-
-    val tag = "more_than_${daysCount}_days"
+enum class StartTimeBucket(val daysCount: Int, val tag: String, val color: Color?) {
+    BUCKET_0(0, "less_than_1_year", null),
+    BUCKET_1(365, "more_than_1_year", Color(56, 186, 78)),
+    BUCKET_2(365 * 3, "more_than_3_years", Color(250, 231, 28)),
+    BUCKET_3(365 * 5, "more_than_5_years", Color(255, 147, 39)),
+    BUCKET_4(365 * 10, "more_than_10_years", Color(255, 0, 0));
 }
 
 fun chooseStartTimeBucket(startDate: LocalDate): StartTimeBucket {
